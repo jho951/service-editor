c33e4efd003932a6ebe013322d505c62c70fe1b7
gyeonghunMin <riverpower6@naver.com>
2026-03-27 17:39:52 +0900

    refactor: 워크스페이스 관련 코드 전부 제거
    1. 해당 코드들은 /backup 패키지에 보관되어있습니다.

--- documents-infrastructure/src/main/java/com/documents/repository/WorkspaceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.documents.repository;
-
-import com.documents.domain.Workspace;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkspaceRepository extends JpaRepository<Workspace, UUID> {
-}
