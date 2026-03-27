34c723883e383d4446094305465c45fbef90ceac
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-03-27 12:58:45 +0900

    Revert "feat: 외부 헬스체크용 API 추가 (#50)"
    This reverts commit 5147f6d60cfd00470b48be56f66e8ce0809dae6d.

--- documents-api/src/main/java/com/documents/api/config/WebMvcConfig.java
+++ documents-api/src/main/java/com/documents/api/config/WebMvcConfig.java
@@ -22,8 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(gatewayAuthInterceptor)
-            .addPathPatterns("/**")
-			.excludePathPatterns("/health");
+            .addPathPatterns("/**");
     }
 
     @Override
